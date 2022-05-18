ad8362d4af949cc2ecd96d0b9ba49ae55d27971f
WANG <wangyan.dev@outlook.com>
2022-05-18 23:10:18 +0800

    init

---
+++ flu3_demo/android/app/src/main/kotlin/com/example/flu3_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flu3_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flu3_demo/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flu3_demo/app/my_app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
