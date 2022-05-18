f4ba4a13b183eaddb08942454e9157770c2a3b58
WANG <wangyan.dev@outlook.com>
2022-05-18 23:57:45 +0800

    test

--- flu3_demo/test/widget_test.dart
+++ flu3_demo/test/widget_test.dart
@@ -5,10 +5,9 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flu3_demo/app/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flu3_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
